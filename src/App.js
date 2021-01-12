385a4c7310ba5c22bb89109cd853a502c13de982
NLI262 <sri.harsha@nineleaps.com>
2021-01-12 18:02:58 +0530

    added env

--- src/App.js
+++ src/App.js
@@ -8,20 +8,7 @@ function App() {
       <h1 className="responsive-header">Responsive With</h1>
       <h1 className="scss-container">SCSS </h1>
       
-      <table className="table-element" >
-        <tr>
-          <th>Month</th>
-          <th>Savings</th>
-        </tr>
-        <tr>
-          <td>January</td>
-          <td>$100</td>
-        </tr>
-        <tr>
-          <td>February</td>
-          <td>$501</td>
-        </tr>
-      </table>
+  <h1 className="responsive-header">{process.env.REACT_APP_VALUE}</h1>
       </div>
     
   );
