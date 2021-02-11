75cd4207c9bd5cc987469077c0ee82f82e6ede89
NLI262 <sri.harsha@nineleaps.com>
2021-02-11 12:43:47 +0530

    [device emulator poc]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import DeviceEmulator from 'react-device-emulator';
+import 'react-device-emulator/lib/styles/style.css';
 import './App.scss';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="responsive-header">Responsive With</h1>
-      <h1 className="scss-container"> SCSS  </h1>
-      
-  
-      </div>
+    <div>
+    <DeviceEmulator  type="mobile" withDeviceSwitch withRotator >
+      <h1 style={{color:"white"}}>Welcome to React Device Emulator</h1>
+    </DeviceEmulator>
+   
+  </div>
     
   );
 }
